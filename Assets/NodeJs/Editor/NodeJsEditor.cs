60b787acaf5b756d0c87a48bb889754da6a94d13
Derek Severin <severinderek@gmail.com>
2019-04-09 21:57:16 +0700

    Functional v0.1

---
+++ Assets/NodeJs/Editor/NodeJsEditor.cs
@@ -0,0 +1,193 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CustomEditor(typeof(NodeJs))]
+public sealed class NodeJsEditor : Editor {
+
+    SerializedProperty useNodeEmbeddedProp;
+    SerializedProperty useNodeDefaultProp;
+    SerializedProperty nodePathProp;
+
+    SerializedProperty useScriptPathDefaultProp;
+    SerializedProperty scriptPathProp;
+    SerializedProperty scriptNameProp;
+    SerializedProperty scriptArgumentsProp;
+
+    bool showNode = false;
+    bool showScript = true;
+
+
+    //TODO: should use "InitializeOnLoad"?
+    private void OnEnable() {
+        useNodeEmbeddedProp = serializedObject.FindProperty("useNodeEmbedded");
+        useNodeDefaultProp = serializedObject.FindProperty("useNodeDefault");
+        nodePathProp = serializedObject.FindProperty("nodePath");
+//TODO: check that not null!
+
+        EditorPrefs.SetBool("useNodeEmbedded", useNodeEmbeddedProp.boolValue);
+        EditorPrefs.SetBool("useNodeDefault", useNodeDefaultProp.boolValue);
+        EditorPrefs.SetString("nodePath", nodePathProp.stringValue);
+
+
+        useScriptPathDefaultProp = serializedObject.FindProperty("useScriptPathDefault");
+        scriptPathProp = serializedObject.FindProperty("scriptPath");
+        scriptNameProp = serializedObject.FindProperty("scriptName");
+        scriptArgumentsProp = serializedObject.FindProperty("scriptArguments");
+//TODO: check that not null!
+
+        EditorPrefs.SetBool("useScriptPathDefault", useScriptPathDefaultProp.boolValue);
+        EditorPrefs.SetString("scriptPath", scriptPathProp.stringValue);
+        EditorPrefs.SetString("scriptName", scriptNameProp.stringValue);
+        EditorPrefs.SetString("scriptArguments", scriptArgumentsProp.stringValue);
+    }
+
+
+    public override void OnInspectorGUI() {
+
+        serializedObject.Update();
+
+
+        showNode = EditorGUILayout.Foldout(showNode, "Node.js");
+        if (showNode) {
+            EditorGUI.indentLevel++;
+
+/*
+        EditorGUI.BeginChangeCheck();
+        EditorGUILayout.PropertyField(useNodeProvidedProp, new GUIContent("Use provided Node.js"));
+        if (EditorGUI.EndChangeCheck()) {
+            EditorPrefs.SetBool("useNodeProvided", useNodeProvidedProp.boolValue);
+        }
+
+        if (!useNodeProvidedProp.boolValue) {
+            EditorGUI.indentLevel++;
+
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(useNodeSystemProp, new GUIContent("Use system Node.js"));
+            if (EditorGUI.EndChangeCheck()) {
+                EditorPrefs.SetBool("useNodeSystem", useNodeSystemProp.boolValue);
+            }
+
+            if (!useNodeSystemProp.boolValue) {
+
+                EditorGUI.BeginChangeCheck();
+                EditorGUILayout.PropertyField(nodePathProp, new GUIContent("Node.js path"));
+                if (EditorGUI.EndChangeCheck()) {
+                    EditorPrefs.SetString("nodePath", nodePathProp.stringValue);
+                }
+//TODO: change this if can use a script to copy external files (Python script?)
+                EditorGUILayout.HelpBox("The specified path must be located in the 'StreamingAssets' directory or a subdirectory, and contain the Node.js executable.\n"
+                        + "It will be included in the generated build", MessageType.Warning);
+            }
+            else {
+                EditorGUILayout.HelpBox("The device running the application will need to have a Node.js installed and included in the system's path variable!", MessageType.Warning);
+            }
+            EditorGUI.indentLevel--;
+        }
+*/
+        EditorGUI.BeginChangeCheck();
+        EditorGUILayout.PropertyField(useNodeEmbeddedProp, new GUIContent("Use embedded Node.js"));
+        if (EditorGUI.EndChangeCheck()) {
+            EditorPrefs.SetBool("useNodeEmbedded", useNodeEmbeddedProp.boolValue);
+        }
+
+        if (useNodeEmbeddedProp.boolValue) {
+            EditorGUI.indentLevel++;
+
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(useNodeDefaultProp, new GUIContent("Use default path"));
+            if (EditorGUI.EndChangeCheck()) {
+                EditorPrefs.SetBool("useNodeDefault", useNodeDefaultProp.boolValue);
+            }
+
+            if (!useNodeDefaultProp.boolValue) {
+
+                EditorGUI.BeginChangeCheck();
+                EditorGUILayout.PropertyField(nodePathProp, new GUIContent("Node.js relative path"));
+                if (EditorGUI.EndChangeCheck()) {
+                    EditorPrefs.SetString("nodePath", nodePathProp.stringValue);
+                }
+//TODO: change this if can use a script to copy external files (Python script?)
+                EditorGUILayout.HelpBox("The specified path must be located in the 'StreamingAssets' directory or a subdirectory, and contain the Node.js executable.\n"
+                        + "It will be included in the generated build", MessageType.Warning);
+            }
+            else {
+//TODO: keep this?
+                EditorGUILayout.HelpBox("The default path for Node.js is \"" + NodeJs.NODE_DEFAULT_PATH + "\"", MessageType.Info);
+            }
+            EditorGUI.indentLevel--;
+        }
+        else {
+            EditorGUILayout.HelpBox("The device running the application will need to have a Node.js installed and included in the system's path variable!", MessageType.Warning);
+        }
+
+            EditorGUI.indentLevel--;
+        }
+
+
+        var nodejs = target as NodeJs;
+
+        showScript = EditorGUILayout.Foldout(showScript, "JS Script");
+        if (showScript) {
+            EditorGUI.indentLevel++;
+
+            EditorGUI.BeginChangeCheck();
+        EditorGUILayout.PropertyField(useScriptPathDefaultProp, new GUIContent("Use default script path"));
+        if (EditorGUI.EndChangeCheck()) {
+            EditorPrefs.SetBool("useScriptPathDefault", useScriptPathDefaultProp.boolValue);
+        }
+
+            if (!useScriptPathDefaultProp.boolValue) {
+                EditorGUI.BeginChangeCheck();
+                EditorGUILayout.PropertyField(scriptPathProp, new GUIContent("Relative path"));
+                if (EditorGUI.EndChangeCheck()) {
+                    EditorPrefs.SetString("scriptPath", scriptPathProp.stringValue);
+                }
+                EditorGUILayout.HelpBox("The specified path must be located in the 'StreamingAssets' directory or a subdirectory", MessageType.Warning);
+            }
+            else {
+//TODO: keep this?
+                EditorGUILayout.HelpBox("The default path for scripts is \"" + NodeJs.SCRIPT_DEFAULT_PATH + "\"", MessageType.Info);
+            }
+
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(scriptNameProp, new GUIContent("Name"));
+            if (EditorGUI.EndChangeCheck()) {
+                EditorPrefs.SetString("scriptName", scriptNameProp.stringValue);
+            }
+
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(scriptArgumentsProp, new GUIContent("Arguments"));
+            if (EditorGUI.EndChangeCheck()) {
+                EditorPrefs.SetString("scriptArguments", scriptArgumentsProp.stringValue);
+            }
+
+            EditorGUI.indentLevel--;
+        }
+
+
+//TODO: DO AS REST => PROPERTYFIELDS! (else problems of persistency)
+//TODO: rewrite?
+        var logLength = EditorGUILayout.IntField("Log Length", nodejs.logLength);
+		if (logLength != nodejs.logLength) {
+			nodejs.logLength = logLength;
+		}
+
+//TODO: should be optional
+		EditorGUILayout.LabelField("Output Log");
+		EditorGUI.indentLevel++;
+		EditorGUILayout.TextArea(nodejs.log);
+		EditorGUI.indentLevel--;
+
+//TODO: REMOVE!
+        var text = EditorGUILayout.ObjectField("text", nodejs.text, typeof(Text));
+        if (text != nodejs.text)
+            nodejs.text = (Text)text;
+
+
+        serializedObject.ApplyModifiedProperties();
+
+    }
+
+}
+
---
+++ Assets/NodeJs/Scripts/NodeJs.cs
@@ -0,0 +1,183 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+
+public class NodeJs : MonoBehaviour {
+
+//TODO: REMOVE!
+    public Text text;
+
+
+	#region [ static members ]
+	public static readonly string NODE_DEFAULT_PATH = ".node";
+//TODO: Linux? Android?
+	#if UNITY_STANDALONE_WIN
+	private static readonly string NODE_BIN = "node.exe";
+	#endif
+	#if UNITY_STANDALONE_OSX
+//TODO: change
+//=> ADD IOS BINARY (portable?)
+	private static readonly string NODE_BIN = "node";
+	#endif
+
+	public static readonly string SCRIPT_DEFAULT_PATH = ".script";
+    #endregion
+
+    #region [ member variables ]
+    public bool useNodeEmbedded = true;
+    public bool useNodeDefault = true;
+    public string nodePath = "";
+
+    public bool useScriptPathDefault = true;
+    public string scriptPath = "";
+    public string scriptName = "";
+    public string scriptArguments = "";
+
+//TODO: ?
+    public int logLength = 10;
+//TODO: ?
+    public string log = "";
+
+
+    private string nodeFullPath;
+    private string scriptFullPath;
+    private bool isInitialized = false;
+
+    private System.Diagnostics.Process process_ = null;
+	private List<string> logs_ = new List<string>();
+	#endregion
+
+	#region [ getter / setter ]
+	public bool isRunning { get; set; }
+	#endregion
+
+	#region [ member functions ]
+	private void Init() {
+
+        if (!isInitialized) {
+            if (useNodeEmbedded) {
+                if (useNodeDefault) {
+                    nodePath = NODE_DEFAULT_PATH;
+                    nodeFullPath = System.IO.Path.Combine(Application.streamingAssetsPath, nodePath);
+                }
+                else {
+                    nodeFullPath = System.IO.Path.Combine(Application.streamingAssetsPath, nodePath);
+                }
+            }
+            else {
+                nodePath = "";
+                nodeFullPath = "";
+            }
+//print("nodeFullPath: " + nodeFullPath);
+
+
+//TODO: allow possibility to use external script...
+            if (useScriptPathDefault) {
+                scriptPath = SCRIPT_DEFAULT_PATH;
+                scriptFullPath = System.IO.Path.Combine(Application.streamingAssetsPath, scriptPath);
+            }
+            else {
+                scriptFullPath = System.IO.Path.Combine(Application.streamingAssetsPath, scriptPath);
+            }
+//print("scriptFullPath: " + scriptFullPath);
+
+            isInitialized = true;
+		}
+	}
+
+	void Awake() {
+		isRunning = false;
+
+        Init();
+
+        try {
+            Run();
+        }
+        catch (System.Exception e) {
+            Debug.LogException(e, this);
+            if (process_ != null) {
+                process_.Dispose();
+                process_ = null;
+            }
+            Application.Quit(-1);
+        }
+	}
+
+	void OnDestroy() {
+		if ((process_ != null) && !process_.HasExited) {
+			process_.Kill();
+			process_.Dispose();
+		}
+	}
+
+	void Run() {
+		if (isRunning) {
+			Debug.LogError("Already Running: " + scriptName);
+			return;
+		}
+
+        InitProcess();
+
+        isRunning = true;
+	}
+
+    void InitProcess() {
+
+        process_ = new System.Diagnostics.Process();
+
+        process_.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+        process_.StartInfo.FileName = System.IO.Path.Combine(nodeFullPath, NODE_BIN);
+        process_.StartInfo.Arguments = System.IO.Path.Combine(scriptFullPath, scriptName) + " " + scriptArguments;
+        process_.StartInfo.CreateNoWindow = true;
+        process_.StartInfo.RedirectStandardOutput = true;
+        process_.StartInfo.RedirectStandardError = true;
+        process_.StartInfo.UseShellExecute = false;
+        process_.StartInfo.WorkingDirectory = scriptFullPath;
+
+        process_.OutputDataReceived += OnOutputData;
+        process_.ErrorDataReceived += OnOutputData;
+        process_.EnableRaisingEvents = true;
+        process_.Exited += OnExit;
+
+        print("Starting: " + process_.StartInfo.FileName + " " + process_.StartInfo.Arguments);
+text.text = "Starting: " + process_.StartInfo.FileName + " " + process_.StartInfo.Arguments;
+        process_.Start();
+
+        process_.BeginOutputReadLine();
+        process_.BeginErrorReadLine();
+    }
+
+
+    void Stop() {
+		if (!isRunning) {
+			Debug.LogError("Already Stopping: " + scriptName);
+			return;
+		}
+
+//TODO: should check that not null?
+        process_.Kill();
+		process_.Dispose();
+	}
+
+//TODO: rewrite... should be optional...?
+	void OnOutputData(object sender, System.Diagnostics.DataReceivedEventArgs e) {
+		if (logs_.Count > logLength) {
+            logs_.RemoveAt(0);
+        }
+        logs_.Add(e.Data);
+        log = "";
+        logs_.ForEach(line => { log += line + "\n"; });
+Debug.Log("LOG: " + log);
+text.text += log;
+	}
+
+	void OnExit(object sender, System.EventArgs e) {
+		isRunning = false;
+		if (process_.ExitCode != 0) {
+			Debug.LogError("Error! Exit Code: " + process_.ExitCode);
+		}
+	}
+    #endregion
+
+}
